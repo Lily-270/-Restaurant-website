ce2937f27a643764c2c5d647c365052eb16faa76
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2025-03-02 00:17:07 +0300

    .

---
+++ project/src/components.js
@@ -0,0 +1,43 @@
+function Nav() {
+    return (
+        <>
+            <h2>Lilyan</h2>
+        </>
+    )
+}
+function Header() {
+    return (
+        <>
+            <h1>Hatim</h1>
+        </>
+    )
+}
+function Main() {
+    return (
+        <>
+            <h2>Lilyan</h2>
+        </>
+    )
+}
+function Section() {
+    return (
+        <>
+            <h2>Lilyan</h2>
+        </>
+    )
+}
+function Article() {
+    return (
+        <>
+            <h2>Lilyan</h2>
+        </>
+    )
+}
+function Footer() {
+    return (
+        <>
+            <h2>Lilyan</h2>
+        </>
+    )
+}
+export { Header, Nav, Main, Section, Article, Footer };
