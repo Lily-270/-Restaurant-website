311bc952834b6b72860142e06b319faf9123e44b
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2025-03-10 14:47:12 +0300

    /

--- project/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'), // Use @tailwindcss/postcss as a plugin
-    require('autoprefixer'), // Optional: for automatic vendor prefixes
-  ],
-};
-
