8864970fd48c658052ba040fe83ab4101f236724
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2025-03-12 12:15:01 +0300

    updated
    added new sections

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import Nav from './Nav.js'
 import Header from './Heading.js'
+import Specials from './Specials.js'
 function App() {
   return (
     <div>
       <Nav/>
       <Header/>
+      <Specials/>
   </div>
   );
 }
